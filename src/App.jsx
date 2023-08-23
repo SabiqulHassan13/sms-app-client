541e4569432b04cfca93a24e1c7ee0d7818bb237
SabiqulHassan13 <sabiqulhassan98@gmail.com>
2023-08-23 19:33:02 +0600

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,23 @@ function App() {
   return (
     <>
       <div>
-        <h2>hello world</h2>
+        <h2>Login Form</h2>
+
+        <form action="" method="">
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" required />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" required />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+
+        <p>
+          No account yet? Please register <a href="/register">here</a>
+        </p>
       </div>
     </>
   );
